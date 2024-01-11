74d2d9305a50998771e0468f7ff1418fd80597e0
Gaurav <gvaghasiya02@gmail.com>
2024-01-12 03:05:03 +0530

    original dataset

--- wisconload.py
+++ wisconload.py
@@ -12,7 +12,7 @@ csv_filename = "query_metrics.csv"
 
 # Define data for the query
 data = {
-"statement": "drop dataverse wiscon if exists; create dataverse wiscon;USE wiscon;CREATE TYPE wiscondeftype AS {unique1: int,unique2: int,two: int,four:int,ten:int,twenty:int,onePercent:int,tenPercent:int,twentyPercent:int,fiftyPercent: int,unique3:int,evenOnePercent:int,oddOnePercent:int,stringu1: string,stringu2: string,string4:string};CREATE DATASET wiscondef(wiscondeftype) PRIMARY KEY unique2;LOAD DATASET wiscondef USING localfs (('path' = 'localhost:///home/dbis-nuc10/DBIS/data/wiscondatadef.adm'),  ('format' = 'adm'));",
+"statement": "drop dataverse wiscon if exists; create dataverse wiscon;USE wiscon;CREATE TYPE wiscondeftype AS {unique1: int,unique2: int,two: int,four:int,ten:int,twenty:int,onePercent:int,tenPercent:int,twentyPercent:int,fiftyPercent: int,unique3:int,evenOnePercent:int,oddOnePercent:int,stringu1: string,stringu2: string,string4:string};CREATE DATASET wiscondef(wiscondeftype) PRIMARY KEY unique2;LOAD DATASET wiscondef USING localfs (('path' = 'localhost:///home/dbis-nuc10/DBIS/data/wiscon107.adm'),  ('format' = 'adm'));",
     "pretty": "true",
     "client_context_id": "xyz"
 }
