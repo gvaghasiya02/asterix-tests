c75a80196df337594affc3e53347fa135cff91ab
Gaurav <gvaghasiya02@gmail.com>
2024-01-12 01:52:13 +0530

    gg

--- onetimesetup.sh
+++ onetimesetup.sh
@@ -5,11 +5,11 @@ python3 --version
 git --version
 wait
 rm -rf asterixdb_gaurav data JSON-Wisconsin-Data-Generator
-echo "cloning asterixdb"
-git clone https://github.com/gvaghasiya02/asterixdb_gaurav.git
-wait 
 echo "cloning datagen"
 git clone https://github.com/shivajah/JSON-Wisconsin-Data-Generator.git
 wait
 mkdir data
+echo "cloning asterixdb"
+git clone https://github.com/gvaghasiya02/asterixdb_gaurav.git
+wait
 ls -l
