ca355909e07349e1df45e2f4678e3c8c4b39932f
Gaurav <gvaghasiya02@gmail.com>
2024-01-16 02:50:47 +0530

    fix

--- wiscondefsmall.py
+++ wiscondefsmall.py
@@ -8,7 +8,7 @@ headers = {
 
 # Define data for the query
 data = {
-"statement": "drop dataverse wisconsmall if exists; create dataverse wisconsmall;USE wisconsmall;CREATE TYPE wiscondeftype AS {unique1: int,unique2: int,two: int,four:int,ten:int,twenty:int,onePercent:int,tenPercent:int,twentyPercent:int,fiftyPercent: int,unique3:int,evenOnePercent:int,oddOnePercent:int,stringu1: string,stringu2: string,string4:string};CREATE DATASET wiscondefsmall(wiscondeftype) PRIMARY KEY unique2;LOAD DATASET wiscondef USING localfs (('path' = 'localhost:///home/dbis-nuc10/DBIS/data/wiscondef107.adm'),  ('format' = 'adm'));",
+"statement": "drop dataverse wisconsmall if exists; create dataverse wisconsmall;USE wisconsmall;CREATE TYPE wiscondeftype AS {unique1: int,unique2: int,two: int,four:int,ten:int,twenty:int,onePercent:int,tenPercent:int,twentyPercent:int,fiftyPercent: int,unique3:int,evenOnePercent:int,oddOnePercent:int,stringu1: string,stringu2: string,string4:string};CREATE DATASET wiscondefsmall(wiscondeftype) PRIMARY KEY unique2;LOAD DATASET wiscondefsmall USING localfs (('path' = 'localhost:///home/dbis-nuc10/DBIS/data/wiscondef107.adm'),  ('format' = 'adm'));",
     "pretty": "true",
     "client_context_id": "xyz"
 }
