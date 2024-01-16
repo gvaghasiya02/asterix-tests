a941b2deed59b4ba0eae8f40353c497a9db6ce31
Gaurav <gvaghasiya02@gmail.com>
2024-01-16 08:21:33 +0530

    added test query

---
+++ testquery.py
@@ -0,0 +1,35 @@
+import requests
+
+url = "http://localhost:19002/query/service"
+
+headers = {
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+# Define data for the query
+data = {
+"statement": "SET `compiler.optimize.groupby` 'true'; USE wiscon; SELECT * FROM wiscondef LIMIT 1;",
+    "pretty": "true",
+    "client_context_id": "xyz"
+}
+
+response = requests.post(url, headers=headers, data=data)
+print(response.text)
+
+data = {
+"statement": "SET `compiler.optimize.groupby` 'true'; USE wisconsmall; SELECT * FROM wiscondefsmall LIMIT 1;",
+    "pretty": "true",
+    "client_context_id": "xyz"
+}
+
+response = requests.post(url, headers=headers, data=data)
+print(response.text)
+
+data = {
+"statement": "SET `compiler.optimize.groupby` 'true'; USE filler; SELECT * FROM wisconfiller LIMIT 1;",
+    "pretty": "true",
+    "client_context_id": "xyz"
+}
+
+response = requests.post(url, headers=headers, data=data)
+print(response.text)
