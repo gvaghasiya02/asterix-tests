882b8a1a4d0e696120e5c8e72ae97a413a720bb0
Gaurav <gvaghasiya02@gmail.com>
2024-01-17 06:12:39 +0530

    updated script to run both tests

--- trialrun.sh
+++ trialrun.sh
@@ -1,25 +1,5 @@
-cd ~/DBIS
-# sh ./asterix-tests/onetimesetup.sh
-sh ./asterix-tests/runsetup.sh
+cd ~/DBIS/asterix-tests
 wait
-sh ./asterix-tests/wiscondefdatagen.sh $1
+python3 query.py
 wait
-sh ./asterix-tests/asterixdbstop.sh
-wait
-sh ./asterix-tests/asterixdbstart.sh
-wait
-python3 ./asterix-tests/wisconload.py
-wait
-sh ./asterix-tests/asterixdbstop.sh
-wait
-sh ./asterix-tests/asterixdbstart.sh
-wait
-python3 ./asterix-tests/query.py
-wait
-sh ./asterix-tests/asterixdbstop.sh
-wait
-sh ./asterix-tests/asterixdbstart.sh
-wait
-python3 ./asterix-tests/optquery.py
-wait
-sh ./asterix-tests/asterixdbstop.sh
+python3 optquery.py
