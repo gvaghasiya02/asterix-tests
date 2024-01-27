29fa60785c9ea74f14bcb8ef4dcc61923fae0da0
Gaurav <gvaghasiya02@gmail.com>
2024-01-26 16:13:17 -0800

    fixed

--- datageninc.sh
+++ datageninc.sh
@@ -1,25 +1,25 @@
 echo "wisconsin data generation"
 cd ~/DBIS/JSON-Wisconsin-Data-Generator
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=2147483648
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=2048
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef2gb.adm
 wait
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=4294967296
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=4096
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef4gb.adm
 wait
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=8589934592
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=8192
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef8gb.adm
 wait
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=17179869184
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=16384
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef16gb.adm
 wait
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=34359738368
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=32768
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef32gb.adm
 wait
-java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=68719476736
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=65536
 wait
 cp ./outputp_0.adm ~/DBIS/data/wiscondef64gb.adm
