82e443bd73650389f153d6af2d29c432a8323bac
Gaurav <gvaghasiya02@gmail.com>
2024-02-05 08:22:30 -0800

    added 4 cluster test

---
+++ test4nc2dp/runtest.sh
@@ -0,0 +1,7 @@
+sh ~/DBIS/asterix-tests/test4nc2dp/count/runcounttest.sh
+wait
+sh ~/DBIS/asterix-tests/test4nc2dp/sum/runsumtest.sh
+wait
+sh ~/DBIS/asterix-tests/test4nc2dp/max/runmaxtest.sh
+wait
+sh ~/DBIS/asterix-tests/test4nc2dp/min/runmintest.sh
