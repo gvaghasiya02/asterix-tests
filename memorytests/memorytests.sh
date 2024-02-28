4074c2c7271889ddad0e50b549397f0de3087755
Gaurav <gvaghasiya02@gmail.com>
2024-02-27 21:11:23 -0800

    fixed tests

--- memorytests/memorytests.sh
+++ memorytests/memorytests.sh
@@ -1,12 +1,24 @@
-python3 ~/DBIS/asterix-tests/memorytests/count/query_count_16gb.py
+python3 ~/DBIS/asterix-tests/memorytests/query_count_16gb.py
 wait
 mv query_metrics.csv ~/DBIS/results/memorytests/query_metrics_count_16gb.csv
-python3 ~/DBIS/asterix-tests/memorytests/count/optquery_count_16gb.py
+python3 ~/DBIS/asterix-tests/memorytests/optquery_count_16gb.py
 wait
 mv query_metrics.csv ~/DBIS/results/memorytests/opt_query_metrics_count_16gb.csv
-python3 ~/DBIS/asterix-tests/memorytests/count/query_sum_16gb.py
+python3 ~/DBIS/asterix-tests/memorytests/query_sum_16gb.py
 wait
 mv query_metrics.csv ~/DBIS/results/memorytests/query_metrics_sum_16gb.csv
-python3 ~/DBIS/asterix-tests/memorytests/count/optquery_sum_16gb.py
+python3 ~/DBIS/asterix-tests/memorytests/optquery_sum_16gb.py
 wait
 mv query_metrics.csv ~/DBIS/results/memorytests/opt_query_metrics_sum_16gb.csv
+python3 ~/DBIS/asterix-tests/memorytests/query_max_16gb.py
+wait
+mv query_metrics.csv ~/DBIS/results/memorytests/query_metrics_max_16gb.csv
+python3 ~/DBIS/asterix-tests/memorytests/optquery_max_16gb.py
+wait
+mv query_metrics.csv ~/DBIS/results/memorytests/opt_query_metrics_max_16gb.csv
+python3 ~/DBIS/asterix-tests/memorytests/query_min_16gb.py
+wait
+mv query_metrics.csv ~/DBIS/results/memorytests/query_metrics_min_16gb.csv
+python3 ~/DBIS/asterix-tests/memorytests/optquery_min_16gb.py
+wait
+mv query_metrics.csv ~/DBIS/results/memorytests/opt_query_metrics_min_16gb.csv
