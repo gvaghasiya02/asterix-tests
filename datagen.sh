a4060ea65ce7f91bd5da64f9b2bca42a7c6c9c6f
Gaurav <gvaghasiya02@gmail.com>
2024-03-14 12:10:56 -0700

    generating data for more groups

---
+++ datagen.sh
@@ -0,0 +1,25 @@
+echo "wisconsin data generation"
+cd ~/DBIS/JSON-Wisconsin-Data-Generator
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=2048
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_2gb.adm
+wait
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=4096
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_4gb.adm
+wait
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=8192
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_8gb.adm
+wait
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=16384
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_16gb.adm
+wait
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=32768
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_32gb.adm
+wait
+java -jar ./target/wisconsin-datagen.jar writer=file workload=Default.json  filesize=65536
+wait
+cp ./outputp_0.adm ~/DBIS/data/wiscon_moregroups_64gb.adm
