34f7587e8f0cf6dbbbb8a6859d0a4b41c6f5e6fb
Gaurav <gvaghasiya02@gmail.com>
2024-04-23 18:05:45 -0700

    removed max min tests

--- temps/runs.sh
+++ temps/runs.sh
@@ -10,15 +10,3 @@ mv query_metrics.csv ~/DBIS/results/temps/query_metrics_sum_16gb.csv
 python3 ~/DBIS/asterix-tests/temps/optquery_sum_16gb.py
 wait
 mv query_metrics.csv ~/DBIS/results/temps/opt_query_metrics_sum_16gb.csv
-python3 ~/DBIS/asterix-tests/temps/query_max_16gb.py
-wait
-mv query_metrics.csv ~/DBIS/results/temps/query_metrics_max_16gb.csv
-python3 ~/DBIS/asterix-tests/temps/optquery_max_16gb.py
-wait
-mv query_metrics.csv ~/DBIS/results/temps/opt_query_metrics_max_16gb.csv
-python3 ~/DBIS/asterix-tests/temps/query_min_16gb.py
-wait
-mv query_metrics.csv ~/DBIS/results/temps/query_metrics_min_16gb.csv
-python3 ~/DBIS/asterix-tests/temps/optquery_min_16gb.py
-wait
-mv query_metrics.csv ~/DBIS/results/temps/opt_query_metrics_min_16gb.csv
